253606fcce5fe6fad9559ae606e3280a62b626a9
rand0mdev <bechiirr71@gmail.com>
2020-03-17 09:09:49 +0000

    feat: [backend] added authentification

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use \Exception;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Http\Util\TargetPathTrait;
+
+class SecurityController extends AbstractController
+{
+    use TargetPathTrait;
+
+    /**
+     * @Route("/login", name="app_login")
+     */
+    public function login(Request $request, AuthenticationUtils $authenticationUtils): Response
+    {
+        if ($this->getUser()) {
+            if ($targetPath = $this->getTargetPath($request->getSession(), 'main')) {
+                return new RedirectResponse($targetPath);
+            }
+    
+            if($targetPath = $request->query->get('_target_path')) {
+                return new RedirectResponse($targetPath);
+            }
+
+            // return $this->redirectToRoute('index');
+            return new Response('ok');
+        }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    /**
+     * @Route("/logout", name="app_logout")
+     */
+    public function logout()
+    {
+        throw new Exception('This method can be blank - it will be intercepted by the logout key on your firewall');
+    }
+
+    /**
+     * @Route("/request-password", name="app_forgot")
+     */
+    public function forgot()
+    {
+        return $this->render('security/forgot.html.twig');
+    }
+}
