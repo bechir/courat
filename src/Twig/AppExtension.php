7de54618dc84e0b025933b0eeb4ac87fa0973fab
rand0mdev <bechiirr71@gmail.com>
2020-03-18 17:11:11 +0000

    Add rtl support for navbar

---
+++ src/Twig/AppExtension.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Twig;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Intl\Languages;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+use Twig\TwigFunction;
+
+class AppExtension extends AbstractExtension
+{
+    private $container;
+    private $locales;
+    private $localeCodes;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+        $this->localeCodes = explode('|', $container->getParameter('locales'));
+    }
+
+    public function getFilters(): array
+    {
+        return [
+            // If your filter generates SAFE HTML, you should add a third
+            // parameter: ['is_safe' => ['html']]
+            // Reference: https://twig.symfony.com/doc/2.x/advanced.html#automatic-escaping
+            // new TwigFilter('locales', [$this, 'getLocales']),
+        ];
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('locale', [$this, 'getLocale']),
+            new TwigFunction('locales', [$this, 'getLocales']),
+        ];
+    }
+
+    public function getLocale(): ?string
+    {
+        return $this->container->get('request_stack')->getMasterRequest()->attributes->get('_locale');
+    }
+
+    public function getLocales(): array
+    {
+        if(null !== $this->locales) {
+            return $this->locales;
+        }
+
+        $this->locales = [];
+        foreach ($this->localeCodes as $code) {
+            $this->locales[] = [
+                'code' => $code,
+                'name' => Languages::getName($code, $code)
+            ];
+        }
+
+        return $this->locales;
+    }
+}
