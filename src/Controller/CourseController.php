fc1d6e632855ef87c268b2b6a1cf19dfb4d72acb
rand0mdev <bechiirr71@gmail.com>
2020-03-18 17:11:11 +0000

    Add rtl support for navbar

--- src/Controller/CourseController.php
+++ src/Controller/CourseController.php
@@ -3,13 +3,9 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
 
 class CourseController extends AbstractController
 {
-    /**
-     * @Route("/course/{class}", name="course")
-     */
     public function index()
     {
         return $this->render('course/index.html.twig', [
