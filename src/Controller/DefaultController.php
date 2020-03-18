e2a1e956efe3fb71c7f19a5a7099d459f762b166
rand0mdev <bechiirr71@gmail.com>
2020-03-18 17:11:11 +0000

    Add rtl support for navbar

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,13 +4,9 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
-    /**
-     * @Route("/", name="index")
-     */
     public function index(): Response
     {
         return $this->render('default/index.html.twig', [
@@ -18,9 +14,6 @@ class DefaultController extends AbstractController
         ]);
     }
 
-    /**
-     * @Route("/{level}", name="section")
-     */
     public function section($level): Response
     {
         return $this->render('default/index.html.twig', [
