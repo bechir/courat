1909addf54d4aef18b5c88e524e10b82bf005a6d
rand0mdev <bechiirr71@gmail.com>
2020-03-26 17:38:44 +0000

    Added course crud for admin

---
+++ src/Form/CourseType.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of the Rim Edu application.
+ *
+ * By Bechir Ba and contributors
+ */
+
+namespace App\Form;
+
+use App\Entity\Course;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CourseType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('videoUrl')
+            ->add('subject')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Course::class,
+        ]);
+    }
+}
