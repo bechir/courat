ce99237abd14e8f7f6b6c718d6a846fd9b1ce968
rand0mdev <bechiirr71@gmail.com>
2020-03-27 17:30:20 +0000

    Update class levels

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -8,39 +8,13 @@
 
 namespace App\Controller;
 
-use App\Entity\Classe;
-use App\Entity\ClassLevel;
-use App\Repository\ClassRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends AbstractController
 {
-    public function index(EntityManagerInterface $entityManager): Response
+    public function index(): Response
     {
-        $classes = $entityManager->getRepository(Classe::class)->findAll();
-
-        $filter = function ($name) use ($classes) {
-            return array_filter($classes, fn ($class) => $class->getLevel() == $name);
-        };
-
-        $filtered['primaire'] = $filter('primaire');
-        $filtered['college'] = $filter('college');
-        $filtered['lycee'] = $filter('lycee');
-        $filtered['terminale'] = $filter('terminale');
-
-        return $this->render('default/index.html.twig', [
-            'level' => 'all',
-            'classes' => $filtered,
-        ]);
-    }
-
-    public function sectionLevel(ClassLevel $level, ClassRepository $courseClassRepository): Response
-    {
-        return $this->render('default/index.html.twig', [
-            'level' => $level,
-            'classes' => [$level->getName() => $courseClassRepository->findBy(['level' => $level])],
-        ]);
+        return $this->render('default/index.html.twig');
     }
 }
--- src/DataFixtures/ClassFixtures.php
+++ src/DataFixtures/ClassFixtures.php
@@ -9,44 +9,15 @@
 namespace App\DataFixtures;
 
 use App\Entity\Classe;
-use App\Entity\ClassLevel;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class ClassFixtures extends Fixture implements DependentFixtureInterface
+class ClassFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        $level1 = $manager->getRepository(ClassLevel::class)->findOneBy(['name' => 'primaire']);
-        $level2 = $manager->getRepository(ClassLevel::class)->findOneBy(['name' => 'college']);
-        $level3 = $manager->getRepository(ClassLevel::class)->findOneBy(['name' => 'lycee']);
-        $level4 = $manager->getRepository(ClassLevel::class)->findOneBy(['name' => 'terminale']);
-
-        foreach ($this->getPrimaireClasses() as $className) {
+        foreach ($this->getClassNames() as $className) {
             $class = (new Classe())
-                ->setLevel($level1)
-                ->setName($className);
-            $manager->persist($class);
-        }
-
-        foreach ($this->getCollegeClasses() as $className) {
-            $class = (new Classe())
-                ->setLevel($level2)
-                ->setName($className);
-            $manager->persist($class);
-        }
-
-        foreach ($this->getLyceeClasses() as $className) {
-            $class = (new Classe())
-                ->setLevel($level3)
-                ->setName($className);
-            $manager->persist($class);
-        }
-
-        foreach ($this->getTerminaleClasses() as $className) {
-            $class = (new Classe())
-                ->setLevel($level4)
                 ->setName($className);
             $manager->persist($class);
         }
@@ -54,35 +25,15 @@ class ClassFixtures extends Fixture implements DependentFixtureInterface
         $manager->flush();
     }
 
-    public function getPrimaireClasses(): array
-    {
-        return ['1af', '2af', '3af', '4af', '5af', '6af'];
-    }
-
-    public function getCollegeClasses(): array
-    {
-        return ['1as', '2as', '3as', '4as'];
-    }
-
-    public function getLyceeClasses(): array
-    {
-        return ['5c', '5d', '6c', '6d'];
-    }
-
-    public function getTerminaleClasses(): array
+    public function getClassNames(): array
     {
         return [
+            '6af',
+            '4as',
             'TerminaleA',
             'TerminaleC',
             'TerminaleD',
             'TerminaleL',
-        ];
-    }
-
-    public function getDependencies()
-    {
-        return [
-            ClassLevelFixtures::class,
         ];
     }
 }
