c81652b3cb7afa2c1eed4653dc97c1efb428f652
rand0mdev <bechiirr71@gmail.com>
2020-03-27 23:34:23 +0000

    added user creation in admin

---
+++ src/Controller/Admin/UserController.php
@@ -0,0 +1,80 @@
+<?php
+
+/*
+ * This file is part of the Rim Edu application.
+ *
+ * By Bechir Ba and contributors
+ */
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use App\Form\Admin\UserType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+/**
+ * Controller that manage security part of the backend.
+ *
+ * @Route("/admin/user")
+ */
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/", name="admin_users")
+     */
+    public function index(): Response
+    {
+        return $this->render('admin/user/list.html.twig', [
+          'users' => $this->getDoctrine()->getRepository(User::class)->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/", name="admin_user_show")
+     */
+    public function show(User $user): Response
+    {
+        return $this->render('admin/user/show.html.twig', [
+          'user' => $user,
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="admin_user_create")
+     */
+    public function create(Request $request, EntityManagerInterface $em, UserPasswordEncoderInterface $passwordEncoder): Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setPassword(
+                $passwordEncoder->encodePassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+            $user->addRole('ROLE_ADMIN');
+            $em->persist($user);
+
+            $em->flush();
+
+            $this->addFlash('success', "L'utilisateur a été créé avec succès.");
+
+            return $this->redirectToRoute('admin_user_show', [
+                'user' => $user->getId(),
+            ]);
+        }
+
+        return $this->render('admin/user/create.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
---
+++ src/Form/Admin/UserType.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+ * This file is part of the Rim Edu application.
+ *
+ * By Bechir Ba and contributors
+ */
+
+namespace App\Form\Admin;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username')
+            // ->add('roles')
+            ->add('plainPassword', RepeatedType::class, [
+                // instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'type' => PasswordType::class,
+                'first_options' => ['label' => 'Password'],
+                'second_options' => ['label' => 'Repeat Password'],
+                'mapped' => false,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a password',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
