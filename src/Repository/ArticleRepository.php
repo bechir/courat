39c17d94cb6ff31279da681764aca35e2cabb830
rand0mdev <bechiirr71@gmail.com>
2020-04-04 18:59:14 +0000

    Implement article filter

--- src/Repository/ArticleRepository.php
+++ src/Repository/ArticleRepository.php
@@ -14,6 +14,7 @@ namespace App\Repository;
 use App\Entity\Article;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Common\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\ParameterBag;
 
 /**
  * @method Article|null find($id, $lockMode = null, $lockVersion = null)
@@ -26,5 +27,37 @@ class ArticleRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Article::class);
+    }
+
+    public function filter(ParameterBag $query)
+    {
+        $qb = $this->createQueryBuilder('a')
+            ->leftJoin('a.category', 'c')
+                ->addSelect('c')
+            ->leftJoin('a.classe', 'cls')
+                ->addSelect('cls')
+            ->leftJoin('a.subject', 'subj')
+                ->addSelect('subj');
+
+        $subject = $query->getInt('subject', -1);
+        $classe = $query->getInt('classe', -1);
+        $category = $query->getInt('category', -1);
+
+        if ($subject) {
+            $qb->andWhere('subj.id = :subject')
+                ->setParameter('subject', $subject);
+        }
+
+        if ($classe) {
+            $qb->andWhere('cls.id = :classe')
+                ->setParameter('classe', $classe);
+        }
+
+        if ($category) {
+            $qb->andWhere('c.id = :category')
+                ->setParameter('category', $category);
+        }
+
+        return $qb->getQuery()->getResult();
     }
 }
