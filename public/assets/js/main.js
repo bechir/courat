a5531b32d9535f07e1cb30924776b67605f21c51
Athie Mohamed <athiemedlemine@gmail.com>
2020-04-04 23:57:23 +0000

    Added upload page design and added some translations

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -125,3 +125,7 @@ jQuery("i", this).toggleClass("fa-caret-up fa-caret-down");
 });
 
 // Upload
+$(".custom-file-input").on("change", function() {
+  var fileName = $(this).val().split("\\").pop();
+  $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
+});
