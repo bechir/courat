61138eb905d06007a89cf52ebf8e6d44104950ac
rand0mdev <bechiirr71@gmail.com>
2020-04-05 20:12:52 +0000

    Excel support for documents

--- src/Controller/Admin/DocumentController.php
+++ src/Controller/Admin/DocumentController.php
@@ -11,10 +11,19 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Classe;
 use App\Entity\Document;
+use App\Entity\DocumentCategory;
+use App\Entity\ExcelFile;
+use App\Entity\Subject;
+use App\Form\Admin\ExcelFileType;
 use App\Form\DocumentType;
 use App\Repository\DocumentRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use SplFileInfo;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -99,5 +108,106 @@ class DocumentController extends AbstractController
         }
 
         return $this->redirectToRoute('admin_document_index');
+    }
+
+    /**
+     * @Route("/load/excel", name="load_from_excel")
+     */
+    public function loadDocuments(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $excelFile = new ExcelFile();
+        $form = $this->createForm(ExcelFileType::class, $excelFile);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $file = $excelFile->getFile();
+
+            if (!in_array($file->guessClientExtension(), ['csv', 'xlsx', 'xls'])) {
+                $this->addFlash('danger', 'Le fichier excel est invalide: ' . $file->guessClientExtension());
+
+                return $this->redirectToRoute('admin_document_new');
+            }
+
+            $fileName = md5(uniqid()) . '.' . $file->guessClientExtension();
+            $directory = dirname(__FILE__) . '/tmp/';
+
+            try {
+                $file->move($directory, $fileName);
+            } catch (FileException $e) {
+                $this->addFlash('danger', $e->getMessage());
+
+                return $this->redirectToRoute('admin_document_new');
+            }
+
+            $fileName = $directory . '/' . $fileName;
+            $extension = $this->getFileExtension($fileName);
+            $reader = IOFactory::createReader(ucfirst($extension));
+
+            $reader->setReadDataOnly(true);
+            $spreadsheet = $reader->load($fileName);
+            $worksheet = $spreadsheet->getActiveSheet();
+
+            $highestRow = (int) ($worksheet->getHighestDataRow()) - 1;
+
+            try {
+                for ($rowIt = 2; $rowIt <= $highestRow; ++$rowIt) {
+                    $data = $worksheet->rangeToArray(
+                        "A$rowIt:E$rowIt",  // The worksheet range that we want to retrieve
+                        null,               // Value that should be returned for empty cells
+                        true,               // Should formulas be calculated (the equivalent of getCalculatedValue() for each cell)
+                        true,               // Should values be formatted (the equivalent of getFormattedValue() for each cell)
+                        true                // Should the array be indexed by cell row and cell column
+                    )[$rowIt];
+
+                    if (null != $data['A'] && null != $data['B'] && null != $data['C'] && null != $data['D'] && null != $data['E']) {
+                        $document = (new Document())
+                            ->setTitle($data['A'])
+                            ->setFileUrl($data['D']);
+
+                        $class = $entityManager->getRepository(Classe::class)->findOneBy(['name' => $data['B'] ?? null]);
+                        if ($class) {
+                            $class->addDocument($document);
+                        }
+
+                        $subject = $entityManager->getRepository(Subject::class)->findOneBy(['code' => 'subject.' . $data['C'] ?? null]);
+                        if ($subject) {
+                            $document->setSubject($subject);
+                        }
+
+                        $category = $entityManager->getRepository(DocumentCategory::class)->findOneBy(['name' => 'document.category.' . $data['E']]);
+                        if ($category) {
+                            $document->setCategory($category);
+                        }
+
+                        $entityManager->persist($document);
+                    }
+                }
+            } catch (\Exception $e) {
+                throw $e;
+                // $this->addFlash('danger', $e->getMessage());
+
+                // return $this->redirectToRoute('admin_document_new');
+            } finally {
+                unlink($fileName);
+            }
+
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Le fichier a été importé.');
+
+            return $this->redirectToRoute('admin_document_index');
+        }
+
+        return $this->render('admin/common/upload-excel-file.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    public function getFileExtension(string $filename): ?string
+    {
+        $info = new SplFileInfo($filename);
+
+        return $info->getExtension();
     }
 }
