8ffb19bb16287907c1f6db5d4383c2f332cab37b
rand0mdev <bechiirr71@gmail.com>
2020-04-05 19:02:18 +0000

    Fix planning display

--- src/DataFixtures/DocumentCategoryFixtures.php
+++ src/DataFixtures/DocumentCategoryFixtures.php
@@ -35,6 +35,7 @@ class DocumentCategoryFixtures extends Fixture
             'cours',
             'exo',
             'epreuve_bac',
+            'corrige',
             'autre',
         ];
     }
