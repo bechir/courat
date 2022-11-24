3bc178dc7aae9a11770bf4de2e7a1b44f3525056
Bechir Ba <13308700+bechir@users.noreply.github.com>
2022-11-24 18:03:08 +0000

    updated assets dir

--- webpack.config.js
+++ webpack.config.js
@@ -20,10 +20,10 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets_/app.js')
+    .addEntry('app', './assets/app.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
-    .enableStimulusBridge('./assets_/controllers.json')
+    .enableStimulusBridge('./assets/controllers.json')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
